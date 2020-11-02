2b275279d8bb265589b6241810fbaca2254096b2
Oscar Alvarez <oalvarezr@unal.edu.co>
2020-11-02 13:05:24 -0500

    Terminada la funcionalidad de vistas Editar y Registrar usuario, falta arreglar estetica

--- unadopcion/src/main/resources/src/servicio/RegistrarUsuarioServicio.js
+++ unadopcion/src/main/resources/src/servicio/RegistrarUsuarioServicio.js
@@ -6,10 +6,15 @@ const CONTROLADOR = "/crear-usuario";
 class RegistrarUsuarioServicio{
     registrarUsuario(info){
         return axios.post(UNADOPCION_API_URL + CONTROLADOR, {
-            nombre:info.nombre,
-            correo:info.correo,
-            telefono:info.telefono,
-            contrasena:info.contrasena
+            nombre: info.nombre,
+            nombreReal: info.nombreReal,
+            correo: info.correo,
+            telefono: info.telefono,
+            contrasena: info.contrasena,
+            rol: info.rol,
+            lugar: info.lugar,
+            info: info.info,
+            //imagen: info.imagenSeleccionada,
         });
     }
 }
--- unadopcion/src/main/resources/src/views/RegistroUsuario.vue
+++ unadopcion/src/main/resources/src/views/RegistroUsuario.vue
@@ -1,36 +1,199 @@
 <template>
-  <div id="registrousuario"><Header></Header>
-    <br><h1 class="text-center"><strong class="text-capitalize">¡Crea tu cuenta con nosotros!</strong></h1>
-    <br><div class="container">
-    <div class="row justify-content-center">
-      <div class="col-md-8">
+  <div id="registrousuario" onload="">
+    <!--@private byte[] usuarioFoto;-->
+    <Header></Header>
+    <div class="row">
+      <div class="col-7">
+        <br />
+        <!-- primer carta-->
+        <div class="col-8">
+          <div class="card">
+            <div class="card-header">
+              <b>EU Edita tu información para manternos al día!</b>
+            </div>
+
+            <div class="card-body">
+              <form @submit="enviarForma">
+                <div class="row">
+                  <div class="col-4">
+                    <strong>Nickname:</strong>
+                  </div>
+                  <div class="col-8">
+                    <input
+                      type="text"
+                      class="form-control"
+                      @change="comprobarFormato"
+                      v-model="nombre"
+                    />
+                  </div>
+                </div>
+                <br />
+
+                <div class="row">
+                  <div class="col-4">
+                    <strong>Nombre de usuario:</strong>
+                  </div>
+                  <div class="col-8">
+                    <input
+                      type="text"
+                      class="form-control"
+                      @change="comprobarFormato"
+                      v-model="nombreReal"
+                    />
+                  </div>
+                </div>
+                <br />
+
+                <div class="row">
+                  <div class="col-4">
+                    <strong>Contraseña:</strong>
+                  </div>
+                  <div class="col-8">
+                    <input
+                      type="password"
+                      class="form-control"
+                      @change="comprobarFormato"
+                      v-model="contrasena"
+                    />
+                  </div>
+                </div>
+                <br />
+
+                <div class="row">
+                  <div class="col-4">
+                    <strong>Confirme su Contraseña:</strong>
+                  </div>
+                  <div class="col-8">
+                    <input
+                      type="password"
+                      class="form-control"
+                      @change="comprobarFormato"
+                      v-model="contrasena2"
+                    />
+                  </div>
+                </div>
+                <br />
+
+                <div class="row">
+                  <div class="col-4">
+                    <strong>Rol:</strong>
+                  </div>
+                  <div class="col-8">
+                    <select
+                      class="form-control form-control-sm"
+                      @change="comprobarFormato"
+                      v-model="rol"
+                    >
+                      <option>Cuidador</option>
+                      <option>Adoptante</option>
+                      <option>Adoptador</option>
+                    </select>
+                  </div>
+                </div>
+                <br />
+
+                <div class="row">
+                  <div class="col-4">
+                    <strong>Correo:</strong>
+                  </div>
+                  <div class="col-8">
+                    <input
+                      type="text"
+                      class="form-control"
+                      @change="comprobarFormato"
+                      v-model="correo"
+                    />
+                  </div>
+                </div>
+                <br />
+
+                <div class="row">
+                  <div class="col-4">
+                    <strong>Teléfono:</strong>
+                  </div>
+                  <div class="col-8">
+                    <input
+                      type="text"
+                      class="form-control"
+                      @change="comprobarFormato"
+                      v-model="telefono"
+                    />
+                  </div>
+                </div>
+                <br />
+
+                <div class="row">
+                  <div class="col-4">
+                    <strong>Lugar donde vive:</strong>
+                  </div>
+                  <div class="col-8">
+                    <input
+                      type="text"
+                      class="form-control"
+                      @change="comprobarFormato"
+                      v-model="lugar"
+                    />
+                  </div>
+                </div>
+                <br />
+
+                <div class="row">
+                  <div class="col-4">
+                    <strong>Información adicional:</strong>
+                  </div>
+                  <div class="col-8">
+                    <textarea type="text" class="form-control" v-model="info" />
+                  </div>
+                </div>
+                <br />
+
+                <div class="row">
+                  <div class="col-12">
+                    <div class="text-right">
+                      <button class="btn btn-danger mr-4">Cancelar</button>
+                      <button
+                        class="btn btn-success"
+                        id="Aceptar"
+                        @click="enviarForma"
+                      >
+                        Aceptar
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </form>
+              <br />
+              <div class="alert alert-danger" id="error-contraseña">
+                Las contraseñas no coinciden, por favor vuelve a revisar.
+              </div>
+              <div class="alert alert-danger" id="alerta-error">
+                Algún campo obligatorio de datos no posee ningún dato.
+              </div>
+              <br />
+              <strong
+                >Datos actualizados nos permiten mantenerte seguro.</strong
+              >
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- Segunda carta-->
+      <div class="col-4">
+        <br />
         <div class="card">
-          <br><div class="card-header"><b>Registro</b></div>
-
+          <div class="card-header">¡Añade una imagen!</div>
           <div class="card-body">
-            <form @submit="enviarForma">
-              <br><strong>Nombre de usuario:</strong>
-              <input required type="text" class="form-control" v-model="nombre">
-              <br><strong>Tu Nombre:</strong>
-              <input placeholder="Ej:Juan Hernandez" required type="text" class="form-control" v-model="nombreReal">
-              <br><strong>Correo:</strong>
-              <input required type="text" class="form-control" v-model="correo">
-              <br><strong>Teléfono:</strong>
-              <input required type="text" class="form-control" v-model="telefono">
-              <br><strong>Contraseña:</strong>
-              <input required type="password" class="form-control" v-model="contrasena">
-
-              <button class="btn btn-success">Enviar</button>
-            </form>
-            <strong></strong>
-            <pre>
-                        {{probar}}
-            </pre>
+            <br /><strong class="control-label col-sm-2">Fotografia:</strong>
+            <div class="col-sm-10">
+              <input required type="file" @change="enCambioArchivo" />
+            </div>
           </div>
         </div>
       </div>
     </div>
-  </div><footer><Footer></Footer></footer>
+
+    <br />
+    <Footer></Footer>
   </div>
 </template>
 
@@ -50,32 +213,39 @@ export default {
   },
   data() {
     return {
-      nombre: '',
-      nombreReal:'',
-      correo: '',
-      telefono:'',
-      contrasena: '',
-      probar:''
+      nombre: "",
+      nombreReal: "",
+      contrasena: "",
+      contrasena2: "",
+      rol: "",
+      correo: "",
+      telefono: "",
+      lugar: "",
+      info: "",
+      imagenSeleccionada: null,
     };
   },
   methods: {
     enviarForma(e) {
       e.preventDefault();
       let objectoActual = this;
-       let info = {
-         nombre:this.nombre,
-         correo:this.correo,
-         telefono:this.telefono,
-         contrasena:this.contrasena
-      }
+      let info = {
+        nombre: this.nombre,
+        nombreReal: this.nombreReal,
+        contrasena: this.contrasena,
+        rol: this.rol,
+        correo: this.correo,
+        telefono: this.telefono,
+        lugar: this.lugar,
+        info: this.info,
+        imagenSeleccionada: this.imagenSeleccionada
+      };
        this.registrarUsuario(objectoActual, info);
     },
 
     registrarUsuario(objetoActual, info){
       RegistrarUsuarioServicio.registrarUsuario(info)
       .then(respuesta=>{
-        //objetoActual.probar = (respuesta.data);
-        //.log(respuesta.data);
         if(respuesta.status === 201)
         swal.fire("Registro exitoso", "Bienvenido", "success");
       }).catch(error =>{
@@ -83,7 +253,42 @@ export default {
             swal.fire("Intente nuevamente", "El usuario o email ya existe", "error");
           }
       });
-    }
+    },
+
+    enCambioArchivo(event) {
+      this.imagenSeleccionada = event.target.files[0];
+    },
+    comprobarContrasena() {
+      if (this.contrasena != this.contrasena2) {
+        document.getElementById("error-contraseña").style.display = "block";
+        document.getElementById("Aceptar").disabled = true;
+      } else {
+        document.getElementById("error-contraseña").style.display = "none";
+        document.getElementById("Aceptar").disabled = false;
+      }
+    },
+    comprobarDatos() {
+      if (
+        this.nombre == "" ||
+        this.contrasena == "" ||
+        this.rol == "" ||
+        this.correo == "" ||
+        this.nombreReal == "" ||
+        this.telefono == "" ||
+        this.lugar == ""
+      ) {
+        document.getElementById("alerta-error").style.display = "block";
+        document.getElementById("Aceptar").disabled = true;
+        this.comprobarContrasena();
+      } else {
+        document.getElementById("alerta-error").style.display = "none";
+        document.getElementById("Aceptar").disabled = false;
+        this.comprobarContrasena();
+      }
+    },
+    comprobarFormato() {
+      this.comprobarDatos();
+    },
   }
 }
 </script>
